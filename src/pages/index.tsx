b5299487213f8346b4923080c15c2813aeb6f613
yota15 <seasaw.m15@gmail.com>
2023-09-08 00:19:29 +0900

    first_commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,8 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
+            Get started by editing yota;
+            {/* <code className={styles.code}>src/pages/index.tsx</code> */}
           </p>
           <div>
             <a
